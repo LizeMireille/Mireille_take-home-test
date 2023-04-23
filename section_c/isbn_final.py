138e8aad6e6050e494dd8c7ee1d34f342de6ae1c
LizeMireille <127414934+LizeMireille@users.noreply.github.com>
2023-04-23 12:00:40 +0200

    Add files via upload

--- section_c/isbn_final.py
+++ section_c/isbn_final.py
@@ -5,16 +5,12 @@ def check_isbn(isbn_num):
     # Remove dashes commonly seen in ISBN's
     isbn_num = isbn_num.replace("-", "")
 
-    # Disregard inputs with too many or too little digits
-    if len(isbn_num) != 10 and len(isbn_num) != 13:
-        print("Invalid")
-
     # Check if isbn_num is a valid ISBN-13
-    elif len(isbn_num) == 13 and isbn_num.isdigit():
+    if len(isbn_num) == 13 and isbn_num.isdigit():
         # create a list of all the separate digits in isbn_num
         digits = [int(d) for d in str(isbn_num)]
         # create an alternating pattern of 1 and 3 for the multipliers
-        multipliers = [1 if i % 2 == 0 else 3 for i in range(len(digits))]
+        multipliers = [1 if i % 2 == 0 else 3 for i in range(13)]
         # calulate the total
         prod_sum = sum(d * m for d, m in zip(digits, multipliers))
 
@@ -41,12 +37,10 @@ def check_isbn(isbn_num):
         if prod_sum % 11 == 0:
             # convert to ISBN-13 format
             isbn13_num = "978" + isbn_num[:-1]
-
-            # check if isbn13_num is a valid ISBN-13
             # create a list of all the separate digits in isbn13_num
             digits = [int(d) for d in str(isbn13_num)]
             # create an alternating pattern of 1 and 3 for the multipliers
-            multipliers = [1 if i % 2 == 0 else 3 for i in range(len(digits))]
+            multipliers = [1 if i % 2 == 0 else 3 for i in range(13)]
             # calulate the total
             prod_sum = sum(d * m for d, m in zip(digits, multipliers))
             # calculate check digit
